2aab158b84f4f0cfe68efc1f4047574f48406f30
kotlins111 <2757314450@qq.com>
2021-12-03 23:49:56 +0800

    Second commit

---
+++ ArrayDeque.java
@@ -0,0 +1,21 @@
+private void allocateElements(int numElements) {
+        int initialCapacity = MIN_INITIAL_CAPACITY;
+        // Find the best power of two to hold elements.
+        // Tests "<=" because arrays aren't kept full.
+        if (numElements >= initialCapacity) {
+            initialCapacity = numElements;
+            initialCapacity |= (initialCapacity >>>  1);
+            initialCapacity |= (initialCapacity >>>  2);
+            initialCapacity |= (initialCapacity >>>  4);
+            initialCapacity |= (initialCapacity >>>  8);
+            initialCapacity |= (initialCapacity >>> 16);
+            initialCapacity++;
+
+            if (initialCapacity < 0)   // Too many elements, must back off
+                initialCapacity >>>= 1;// Good luck allocating 2 ^ 30 elements
+        }
+        elements = new Object[initialCapacity];
+    }
+/*
+将为 1的高位右移到队列容量原码的最低位
+*/
--- Ds_Algo/LinkedBiTree.cpp
+++ Ds_Algo/LinkedBiTree.cpp
@@ -8,6 +8,16 @@
 using namespace std;
 typedef char TElemType;
 
+#define MAXSIZE 100
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+#define NOTFOUND -3
+
+
 //三叉链表 二叉链表新增一个prior指针 用来寻找父结点地址
 typedef struct TriNode{
     TElemType data;
@@ -20,7 +30,96 @@ typedef struct BiNode{
     BiNode *lchild,*rchild;
 }BiNode,*BiTree;
 
+typedef int Status;
 
+
+typedef BiNode SElemType;
+
+
+//栈的 顺序存储结构体定义
+
+/*top 指向栈顶的上一个元素 base 指向栈底元素 
+ 栈的判空 base==top  
+栈的判满 top-size==stacksize */
+typedef struct {
+    SElemType *base;
+    SElemType *top;
+    int stacksize;
+}SqStack;
+
+
+//顺序栈的初始化
+Status InitStack(SqStack &s){
+    s.base = new SElemType[MAXSIZE];
+    if (!s.base)
+    {
+        /* code */
+        exit(OVERFLOW);
+    };
+    s.top = s.base; //栈顶指针等于栈底指针 空栈
+    s.stacksize =MAXSIZE;
+    return OK;
+}
+
+//栈的判空
+bool StackEmpty(SqStack s){
+
+    return s.base == s.top;
+}
+
+//栈的判满
+bool StackFull(SqStack s){
+    return (s.top-s.base)==s.stacksize;
+}
+//求栈的长度
+int StackLength(SqStack s){
+    return s.top-s.base;
+}
+
+//清空顺序栈
+Status ClearStack(SqStack S){
+    if(S.base){
+        S.top =S.base;
+    }
+    return OK;
+}
+
+//销毁一个顺序栈
+
+Status DestroyStack(SqStack &s){
+    if(s.base){
+        delete s.base; //只是释放了base指针后的一段数组内存空间 指针的指向没有改变
+        s.stacksize=0;
+        s.base=s.top=NULL;
+    }
+    return OK;
+}
+
+/*
+顺序栈的入栈 
+1.判断栈是是否已经满
+2.元素e压入栈顶
+3.top指针上移*/
+Status Push(SqStack &S,SElemType e){
+    if (StackFull(S)) return ERROR;
+    *S.top = e;
+    S.top++; //top指针上移
+    // *S.top++=e;
+    return OK;
+}
+
+/*顺序栈的出栈
+1.栈判空
+2.top--
+3.获取栈顶元素e 
+*/
+Status Pop(SqStack &S,SElemType &e){
+    if(StackEmpty(S)) return ERROR;
+    --S.top;
+    e=*S.top;
+    //e=*--S.top;
+    return OK;
+}
 
 /*
 二叉树的遍历
@@ -34,6 +133,63 @@ typedef struct BiNode{
 3.后序遍历(PostOrderTraverse)
 左子树->右子树->根节点
 */
+void opeartion(BiNode *T);
+Status PreOrderTraverse(BiTree T){
+    if(T==NULL) return OK;
+    else{
+        opeartion(T); 
+        PreOrderTraverse(T->lchild);
+        PreOrderTraverse(T->rchild);
+    }
+    return OK;
+}
+
+Status InOrderTraverse(BiTree T){
+    if(T==NULL) return OK;
+    else{
+        InOrderTraverse(T->lchild);
+        opeartion(T);
+        InOrderTraverse(T->rchild);
+    }
+    return OK;
+}
+
+
+Status PostOrderTraverse(BiTree T){
+    if(T==NULL) return OK;
+    else{
+        PostOrderTraverse(T->lchild);
+        PostOrderTraverse(T->rchild);
+        opeartion(T); 
+    }
+    return OK;
+}
+
+void opeartion(BiNode *T){
+    printf("%d\t",T->data);
+}
+
+
+//非递归遍历二叉树  使用辅助栈
+
+Status InOrderTraverse_(BiTree T){
+    BiTree p,q;
+    SqStack S;
+    InitStack(S);
+    p=T;
+    while (p||!StackEmpty(S))
+    {
+        /* code */
+        if(p){
+            Push(S,p); p=p->lchild;
+        }else{
+            Pop(S,q); opeartion(q);
+            p=q->rchild;
+        }
+    }
+    return OK;
+}
+
 int main(){
     return 0;
 }
