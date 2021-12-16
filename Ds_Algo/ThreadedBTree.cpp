7a5cf07f9060ceee7083b83ff801fde16c68c59d
Sacha <63705410+kotlins111@users.noreply.github.com>
2021-12-16 18:34:52 +0800

    Add files via upload

---
+++ Ds_Algo/ThreadedBTree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+typedef char TElemType;
+typedef int Status;
+
+#define MAXSIZE 100
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+#define NOTFOUND -3
+
+
